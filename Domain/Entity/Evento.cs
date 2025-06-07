a4143f3ee196a382bd2ad8381037c8bdcbe4fc99
Jefferson Guilherme <81498125+jeffersonguilherme@users.noreply.github.com>
2025-06-07 20:37:44 -0300

    Entidade Inscricao
    Criada  a entidade inscricao

--- Domain/Entity/Evento.cs
+++ Domain/Entity/Evento.cs
@@ -6,9 +6,9 @@ public class Evento
     public string Titulo { get; set; }
     public string Descricao { get; set; }
     public string Local { get; set; }
-    public TipoEnum Tipo { get; set; }
+    //public TipoEnum Tipo { get; set; }
     public DateTime DataCriacao { get; set; } = DateTime.Now;
     public DateTime DataInicialIncricao { get; set; } = DateTime.Now;
     public DateTime DataFinalIncricao { get; set; }
-    public ICollection<Inscricao> Inscricoes { get; set; }
+    //public ICollection<Inscricao> Inscricoes { get; set; }
 }
---
+++ Domain/Entity/Inscricao.cs
@@ -0,0 +1,11 @@
+namespace Domain.Entity;
+
+public class Inscricao
+{
+    public Guid Id { get; set; }
+    public Guid EventoId { get; set; }
+    public Evento Evento { get; set; }
+    public Guid ParticipanteId { get; set; }
+    public Participante Participante { get; set; }
+    public DateTime DataInscricao { get; set; } = DateTime.Now;
+}
