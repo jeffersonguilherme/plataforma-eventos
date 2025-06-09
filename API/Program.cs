9db2f028671519c02b1be1151023a3e97d5f0524
Jefferson Guilherme <81498125+jeffersonguilherme@users.noreply.github.com>
2025-06-08 23:53:11 -0300

    Criados diretorios ParcipanteCQ

--- API/Program.cs
+++ API/Program.cs
@@ -1,3 +1,4 @@
+using Application.ParticipanteCQ.Commands;
 using Infra.Persistence;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -6,6 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<EventoDbContext>();
+builder.Services.AddMediatR(config => config.RegisterServicesFromAssemblies(typeof(CreateParticipanteCommand).Assembly));
 
 var app = builder.Build();
 
---
+++ Application/ParticipanteCQ/Commands/CreateParticipanteCommand.cs
@@ -0,0 +1,6 @@
+namespace Application.ParticipanteCQ.Commands;
+
+public record CreateParticipanteCommand
+{
+    
+}
